d22e806877f940c7993b4767b6c198fc8c92df4d
Newburu <newburu@gmail.com>
2023-08-06 22:32:11 +0900

    Cardを追加

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,14 @@ module Yosegaki
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators.template_engine = :slim
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    # デフォルトのlocaleを日本語(:ja)にする
+    config.i18n.default_locale = :ja
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_06_065733) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_06_083801) do
+  create_table "cards", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "dear_name"
+    t.string "title"
+    t.string "image"
+    t.date "open_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cards_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -36,4 +47,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_06_065733) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cards", "users"
 end
