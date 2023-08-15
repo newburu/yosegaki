45735397a8e56384f717a6efe8c2977881db2303
にゅ〜ぶる <newburu@gmail.com>
2023-08-15 14:15:28 +0900

    権限制御を追加する #9 (#10)

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,8 @@
 class Card < ApplicationRecord
   belongs_to :user
+  belongs_to :owner, class_name: 'User', foreign_key: :user_id
+
   has_many :messages
   mount_uploader :image, ImageUploader
+
 end
---
+++ app/policies/card_policy.rb
@@ -0,0 +1,40 @@
+class CardPolicy < ApplicationPolicy
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    user.present?
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    record.owner == user || user.try(:admin?)
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    update?
+  end
+
+  class Scope < Scope
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        scope.where(user_id: user.id)
+      end
+    end
+  end
+
+end
