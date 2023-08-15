09cd6b0ac71d3dab1f79cf9c4b433673751433f8
Newburu <newburu@gmail.com>
2023-08-15 16:04:38 +0900

    Userにroleを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_15_025215) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_15_065613) do
   create_table "cards", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "dear_name"
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_025215) do
     t.string "access_token"
     t.string "access_token_secret"
     t.string "image"
+    t.integer "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
