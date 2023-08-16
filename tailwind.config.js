306a9706e3f5236d03143ea29d938e78c97fc134
Newburu <newburu@gmail.com>
2023-08-16 23:24:07 +0900

    tailwindの設定を変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,12 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}',
-    "./config/initializers/simple_form.rb",
+      './app/views/**/*.html.slim',
+      './app/helpers/**/*.rb',
+      './app/assets/stylesheets/**/*.css',
+      './app/javascript/**/*.js'
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-    require("daisyui"),
-  ],
+  plugins: [require("daisyui")],
   daisyui: {
-    themes: ["dark", "cupcake"],
+      themes: ["dark", "cupcake"],
   },
 }
