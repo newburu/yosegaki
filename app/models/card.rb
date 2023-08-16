4c9ed73506fc8d44cdec1f879d7b4273af4da3af
Newburu <newburu@gmail.com>
2023-08-16 15:35:25 +0900

    Card削除でMessageも削除するように修正

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,7 +2,7 @@ class Card < ApplicationRecord
   belongs_to :user
   belongs_to :owner, class_name: 'User', foreign_key: :user_id
 
-  has_many :messages
+  has_many :messages, dependent: :destroy
   mount_uploader :image, ImageUploader
 
 end
